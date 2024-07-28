ec9c3c3c988ea5576ad5b7fa2c53c476368563c9
ElioCi <107808771+ElioCi@users.noreply.github.com>
2024-07-28 14:55:16 +0200

    Update loginfb.py

--- loginfb.py
+++ loginfb.py
@@ -2,7 +2,25 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("heatloss-69a51-firebase-adminsdk-d8rr6-d86c640f76.json")
+import os
+import json
+
+firebase_config = {
+    'type': os.getenv('TYPE'),
+    'projectId': os.getenv('PROJECT_ID'),
+    'private_key_id': os.getenv('PRIVATE_KEY_ID'),
+    'private_key': os.getenv('PRIVATE_KEY'),
+    'client_email': os.getenv('CLIENT_EMAIL'),
+    'client_id': os.getenv('CLIENT_ID'),
+    'auth_uri': os.getenv('AUTH_URI'),
+    'token_uri':os.getenv('TOKEN_URI'),
+    'auth_provider_x509_cert_url': os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+    'client_x509_cert_url': os.getenv('CLIENT_X509_CERT_URL'),
+    'universe_domain': os.getenv('UNIVERSE_DOMAIN')
+}
+
+cred = credentials.Certificate(firebase_config)
+
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
